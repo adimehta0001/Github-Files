2fd1663c3dad908253e68020ad8536e63c970a1a
Aditya Mehta <adimehta0006@gmail.com>
2024-09-22 10:22:26 +0530

    Aditya Mehta VIPS

---
+++ wap to allot grades based on marks (switch statement).c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    char grade;
+    printf("Enter your Grade");
+    scanf("%c",&grade);
+    switch (grade)
+    {
+        case 'A':
+        printf("Fantastic \n");
+        break;
+        case 'B':
+        printf("Good\n");
+        break;
+        case 'C':
+        printf("Fair Enough\n");
+        break ;
+        case 'D':
+        printf("Need To Improve\n");
+        break;
+        case 'E':
+        printf("Failed\n");
+        break ; 
+    }
+}
